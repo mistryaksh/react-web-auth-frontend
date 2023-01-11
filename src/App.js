6726c3610b48523a98e563ef95f195b00ac6127e
Aakash-mistry <111272027+mistryaksh@users.noreply.github.com>
2023-01-11 18:19:11 +0530

    first uploads

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+     const [email, setEmail] = useState("mistryaksh1998@gmail.com");
+     const [password, setPassword] = useState("abc123");
+
+     const [success, setSuccess] = useState("");
+     const [error, setError] = useState("");
+
+     const sendingLoginRequest = async () => {
+          try {
+               console.log("data", { email, password });
+               const userCredentials = { email, password };
+               const data = await axios.post("http://localhost:8080", userCredentials);
+               setSuccess(data.data.message);
+               setError("");
+               return data.data;
+          } catch (err) {
+               if (err.response) {
+                    setError(err.response.data.message);
+                    setSuccess("");
+               } else {
+                    setError(err.message);
+                    setSuccess("");
+               }
+          }
+     };
+
+     return (
+          <div className="container">
+               <div style={{ width: "40%", padding: 10, margin: "auto" }}>
+                    <input
+                         type="email"
+                         value={email}
+                         onChange={(e) => setEmail(e.target.value)}
+                         name="email"
+                         placeholder="Enter email address"
+                    />
+                    <br />
+                    <br />
+                    <input
+                         type="password"
+                         value={password}
+                         onChange={(e) => setPassword(e.target.value)}
+                         name="password"
+                         placeholder="Enter your password"
+                    />
+                    <br />
+                    <br />
+                    <p className="success">{success && success}</p>
+                    <p className="error">{error && error}</p>
+                    <button onClick={sendingLoginRequest} type="submit">
+                         Login
+                    </button>
+               </div>
+          </div>
+     );
 }
 
 export default App;
